68a7aab7fcce12f06830d1bca60fdc01cfe80d75
jabloco <zefir4eg@gmail.com>
2024-02-24 20:53:47 +0300

    наконец то получил файл

--- journal_client.py
+++ journal_client.py
@@ -1,50 +1,40 @@
 import requests
 
 from settings import TOKEN
+from settings import USER
+from settings import PASSWD
 from datetime import datetime
-# https://one.43edu.ru/edv/index/report/diary/TOKEN?format=xls&date=date&homework-mode=previous
-# https://one.43edu.ru/#diary
-
-
-"""
-import requests
-def diary():
-    session = requests.Session()
-    headers = {'Referer': 'https://www.mos.ru/pgu/ru/application/dogm/journal/'}
-    auth_url = "https://mrko.mos.ru/dnevnik/services/index.php"
-    auth_req = session.get(auth_url, headers=headers, params={"login": ЛОГИН, "password": ПАРОЛЬ_В_MD5}, allow_redirects=False)
-
-    main_req = session.get("https://mrko.mos.ru/dnevnik/services/dnevnik.php?r=1&first=1")
-
-"""
 
 dt_now = datetime.now()
 today = dt_now.strftime('%d.%m.%Y')
+user_agent_val = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+
 print(today)
 
 
 class get_journal:
-    URL_BASE = "https://one.43edu.ru/edv/index/report/diary/"
+    URL_REFERER = "https://one.43edu.ru/auth/login"
+    URL_AUTH = "https://passport.43edu.ru/auth/login"
     URL_TOKEN = TOKEN
     URL_PARAM_FORMAT = "xls"
     URL_PARAM_MODE = "homework-mode=previous"
-
+    
     def get_xls(self):
         """
         Метод для получения xls файла
         Файл небольшой, храним в памяти
-        Необходимо передавать дату
         """
+        auth_data = {'login': USER, 'password': PASSWD, "submit": "submit", "returnTo": "https://one.43edu.ru"}
         session = requests.Session()
-        headers = {'Referer': 'https://one.43edu.ru/#diary'}
-        auth_url = "https://passport.43edu.ru/?returnTo=https%3A%2F%2Fone.43edu.ru" #ссылка на страницу аутентификации
-        url_for_main_req = f"{self.URL_BASE}{self.URL_TOKEN}"
+        auth__req = session.post(self.URL_AUTH, data=data)
+        url_xls_base = f'https://one.43edu.ru/edv/index/report/diary/{self.URL_TOKEN}'
+        url_xls_data = {'format': 'xls',
+                        'date': today,
+                        'homework-mode': 'previous'}
+        get_xls_req = session.get(url_xls_base,data=url_xls_data)
 
-        req = session.get(url_for_main_req,headers=headers, params={"format":self.URL_PARAM_FORMAT, "date": '29.01.2024', "homework-mode":self.URL_PARAM_MODE})
-
-        return req
-
+        return get_xls_req
+    
 journal = get_journal()
-# with open('/media/ssd/python_dev/school_journal/1.xls', 'wb') as f:
-#     f.write(journal)
-print(journal.get_xls().content)
+
+print(journal.get_xls().text)
